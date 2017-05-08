5f21b3bfb065eb0f68697a1fa1036e39946c0422
Calin <calinoiu.alexandru@agilefreaks.com>
2017-05-08 22:17:01 +0300

    Solved coumpound task

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -2,17 +2,21 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter { it.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return orders.filter { it.isDelivered }.flatMap { it.products }.sortedBy { it.price }.firstOrNull()
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    val products = customers
+            .flatMap { it.orders }
+            .flatMap { it.products }
+            .groupBy { it.name }[product.name]
+    return products?.count() ?: 0
 }

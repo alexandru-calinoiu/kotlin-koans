2d96c6e03d185ef1dd6fbf03bbe5e36c9b3fa19d
Calin <calinoiu.alexandru@agilefreaks.com>
2017-05-10 23:00:35 +0300

    Solves date range

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,7 +2,11 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
     override fun compareTo(other: MyDate): Int {
-        return if (year > other.year || month > other.month || dayOfMonth > other.dayOfMonth) 1 else -1
+        return  days() - days(other)
+    }
+
+    private fun days(date: MyDate = this) : Int {
+        return date.year * 365 + date.month * 31 + date.dayOfMonth
     }
 }
 
@@ -14,4 +18,4 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>

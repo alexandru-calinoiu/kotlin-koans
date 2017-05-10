8d0f2f297608b67e2bec1ed4048561ae61ac3c17
Calin <calinoiu.alexandru@agilefreaks.com>
2017-05-10 22:29:42 +0300

    Solves comparison

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,10 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return if (year > other.year || month > other.month || dayOfMonth > other.dayOfMonth) 1 else -1
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 

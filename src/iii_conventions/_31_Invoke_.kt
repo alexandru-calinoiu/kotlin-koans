54e8d65b5f9675ea7f623a8b1a27f0da3aa66267
Calin <calinoiu.alexandru@agilefreaks.com>
2017-05-10 23:39:06 +0300

    Solves invokable

--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,17 +3,24 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(val number: Int = 0) {
+    operator fun invoke(): Invokable {
+        return Invokable(number.inc())
+    }
+
+    fun getNumberOfInvocations(): Int {
+        return number
+    }
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change class Invokable to count the number of invocations (round brackets).
         Uncomment the commented code - it should return 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }

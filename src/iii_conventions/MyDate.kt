2c89d9b7ef78c01ac7a1172123c49f46813e660e
Calin <calinoiu.alexandru@agilefreaks.com>
2017-05-10 23:29:24 +0300

    Solves operator overloading

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,11 +2,19 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
     override fun compareTo(other: MyDate): Int {
-        return  days() - days(other)
+        return days() - days(other)
     }
 
-    private fun days(date: MyDate = this) : Int {
+    private fun days(date: MyDate = this): Int {
         return date.year * 365 + date.month * 31 + date.dayOfMonth
+    }
+
+    operator fun plus(timeInterval: TimeInterval): MyDate {
+        return addTimeIntervals(timeInterval, 1)
+    }
+
+    operator fun plus(timeIntervals: TimeIntervals): MyDate {
+        return addTimeIntervals(timeIntervals.interval, timeIntervals.times)
     }
 }
 
@@ -15,8 +23,14 @@ operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 enum class TimeInterval {
     DAY,
     WEEK,
-    YEAR
+    YEAR;
+
+    operator fun times(i: Int): TimeIntervals {
+        return TimeIntervals(this, i)
+    }
 }
+
+class TimeIntervals(val interval: TimeInterval, val times: Int)
 
 class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
     override fun iterator(): Iterator<MyDate> {
